b7a907a3d67fbb2deb2e72e3c6f4a9daf39d6544
marcos.blandim <marcos.blandim@seatecnologia.com.br>
2021-03-24 00:31:07 -0300

    refact: main.py

--- crowd_detector/main.py
+++ crowd_detector/main.py
@@ -2,42 +2,19 @@ import cv2
 import os
 import logging
 import time
+import yaml
 
-# from detector_utils import detect_crowds
-from yolo_detection import crowd_detect as detect_crowds
-
-
-def fetch_frame(source,max_tries=3):
-    ret = False
-    tries = 0
-
-    while not ret and tries < max_tries:
-        cap = cv2.VideoCapture(source)
-        ret,frame = cap.read()
-        
-        tries += 1
-    
-    cap.release()
-    return frame,ret
+from yolo_detection import detect_crowds
 
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-# settgings
-number_of_frames = 210
-fetch_interval = 3
-max_tries = 3
+settings_filename = "settings.yaml"
+with open(os.path.join(DIR_PATH, settings_filename)) as settings_file:
+    settings = yaml.safe_load(settings_file)
 
-frame_source = "http://208.139.200.133//mjpg/video.mjpg"
-
-filename = "crowd{}.jpg"
-foldername = "result_images"
-
-log_level = "debug"
-
-# setup
-folder_path = os.path.join(DIR_PATH,foldername)
-file_path = os.path.join(folder_path,filename)
+folder_path = os.path.join(DIR_PATH, settings["foldername"])
+file_path = os.path.join(folder_path, settings["filename"])
 
 try:
     os.mkdir(folder_path)
@@ -45,34 +22,52 @@ except FileExistsError:
     pass
 
 logging.basicConfig(
-    level=log_level.upper(),
+    level=settings["log_level"].upper(),
     format='[%(levelname)s] %(asctime)s : "%(message)s"',
 )
 
 
-# main
-logging.info("starting program")
-logging.debug(f"estimated execution time: {(number_of_frames-1)*fetch_interval} seconds")
-try:
+def fetch_frame(source,max_tries=3):
+    '''Tries to fetch one frame from the source max_tries times, and then returns it.
+    '''
+    for i in range(max_tries):
+        logging.debug(f"entering try number {i+1}/{max_tries}")
 
-    frame_counter = 0
-    while frame_counter < number_of_frames:
+        cap = cv2.VideoCapture(source)
+        ret,frame = cap.read()
+        
+        if ret: break
+    
+    if not ret: logging.warning("could not fetch any frame; max tries reached")
 
-        frame, ret = fetch_frame(frame_source,max_tries)
-        if not ret:
-            logging.warning(f"failed at fetching frames, tried {max_tries} times")
-            break
+    cap.release()
+    return frame, ret
 
-        _,marked_frame = detect_crowds(frame)
 
-        cv2.imwrite(file_path.format(frame_counter),marked_frame)
-        logging.info(f"frame {frame_counter+1}/{number_of_frames} saved with success")
+def main():
+    logging.info("starting program")
+    try:
+        for frame_counter in range(settings["number_of_frames"]):
 
-        frame_counter += 1
-        failure_counter = 0
+            frame, ret = fetch_frame(settings["frame_source"], settings["max_tries"])
+            if not ret:
+                max_tries = settings["max_tries"]
+                logging.warning(f"failed at fetching frames, tried {max_tries} times")
+                break
 
-        time.sleep(fetch_interval)
+            _, marked_frame = detect_crowds(frame)
 
-except KeyboardInterrupt:
-    logging.info("KeyboardInterrupt exception, aborting")
-    exit()
+            cv2.imwrite(file_path.format(frame_counter), marked_frame)
+
+            number_of_frames = settings["number_of_frames"]
+            logging.info(f"frame {frame_counter + 1}/{number_of_frames} saved with success")
+
+            time.sleep(settings["fetch_interval"])
+
+    except KeyboardInterrupt:
+        logging.info("KeyboardInterrupt exception, aborting")
+        exit()
+
+if __name__ == "__main__": 
+    main()
+    
